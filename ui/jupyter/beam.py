8631f987afbdb94aa704115f81453cb1ab90758e
Jiao Lin <linjiao@ornl.gov>
2018-02-21 07:00:47 -0500

    arcs beam: converted to a wizard

--- ui/jupyter/beam.py
+++ ui/jupyter/beam.py
@@ -6,11 +6,16 @@
 from __future__ import print_function
 import os
 from ipywidgets import interact, interactive
-import ipywidgets as widgets
+import ipywidgets as ipyw
 from IPython.display import display
 import ipywe.fileselector
+import ipywe.wizard as wiz
 
-def arcs(): return ARCS().createWidget()
+def arcs():
+    context = wiz.Context()
+    step1 = Step1_Parameters(context)
+    step1.show()
+    return context
 
 
 class ARCS:
@@ -20,7 +25,7 @@ class ARCS:
         self.outdir = None
         return
     
-    def g(self, fc, fermi_nu, T0_nu, E, emission_time, ncount, nodes, with_moderator_angling):
+    def setParams(self, fc, fermi_nu, T0_nu, E, emission_time, ncount, nodes, with_moderator_angling):
         params = self.params
         try:
             T0_nu = float(T0_nu)
@@ -37,40 +42,68 @@ class ARCS:
             self.params = dict()
         return
 
-    def createWidget(self):
-        self.form = w = interactive(
-            self.g,
+    def createForm(self):
+        return interactive(
+            self.setParams,
             fc=['100-1.5-SMI', '700-1.5-SMI', '700-0.5-AST'],
             fermi_nu=[600., 480., 360., 300.],
             T0_nu=["60.", "120."],
-            E=widgets.Text("100."),
-            emission_time=widgets.Text("-1."),
-            ncount=widgets.Text("1e7"),
+            E=ipyw.Text("100."),
+            emission_time=ipyw.Text("-1."),
+            ncount=ipyw.Text("1e7"),
             nodes=(1,20),
             with_moderator_angling=True,
             )
-        #
-        outdir_sel= ipywe.fileselector.FileSelectorPanel(
+
+
+class Step1_Parameters(wiz.Step):
+
+    def createPanel(self):
+        self.text = text = ipyw.Text(description="Instrument beam configuration", place_holder='instrument')
+        self.form_factory = ARCS()
+        form = self.form_factory.createForm()
+        OK = ipyw.Button(description='OK')
+        OK.on_click(self.handle_next_button_click)
+        widgets= [form, OK]
+        return ipyw.VBox(children=widgets)
+
+    def validate(self):
+        params = self.form_factory.params
+        # check user input
+        if not params:
+            print("Please check your inputs")
+            return False
+        self.context.params = params # save user input
+        return True
+    
+    def nextStep(self):
+        step2 = Step2_Outdir(self.context)
+        step2.show()
+        
+class Step2_Outdir(wiz.Step):
+    
+    def createPanel(self):
+        self.select = ipywe.fileselector.FileSelectorPanel(
             instruction='select output directory', start_dir=os.path.expanduser('~'), type='directory',
             next=self.on_sel_outdir
         )
-        #
-        self.submit_button = submit_button = widgets.Button(description="Submit")
-        submit_button.on_click(self.simulate)
-        self.panel = panel = widgets.VBox(children=[w, outdir_sel.panel, submit_button])
-        return panel
+        widgets= [self.select.panel]
+        return ipyw.VBox(children=widgets)
 
     def on_sel_outdir(self, selected):
-        self.outdir = selected
-        self.panel.children = [self.form, widgets.Label("Selected output dir: %s" % selected), self.submit_button]
-        return
-
-    def simulate(self, ev):
-        params = self.params
+        self.context.outdir = selected
+        return self.nextStep()
+    
+    def nextStep(self):
+        return self.simulate()
+    
+    def simulate(self):
+        print(self.context)
+        params = self.context.params
         for k, v in params.items():
             print(k,v)
-        print(self.outdir)
+        print(self.context.outdir)
         return
 
-    
+
 # End of file 
