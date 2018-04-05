ba1da6a91331236f315adaea4b8956e2b432d427
Jiao Lin <linjiao@ornl.gov>
2018-04-04 22:05:23 -0400

    sample wizard: mostly done except the last step

--- ui/jupyter/sample.py
+++ ui/jupyter/sample.py
@@ -5,69 +5,114 @@
 
 
 """
-* Workdir
-* Name:
-* Shape: 
 * Material
   * Chemical formula
-  * Unit cell
-* Position/orientation
-* Sample environment
+  * a,b,c,alpha,gamma,beta
+* Shape: 
+  * type
+  * config
+* Workdir
+* Name:
 
 """
 
 from __future__ import print_function
 import os
-from ipywidgets import interact, interactive
 import ipywidgets as ipyw
-from IPython.display import display
-import ipywe.fileselector
 from . import wizard as wiz
 from .Form import FormFactory
 
-def sampleassembly(context=None):
+def sample(context=None):
     context = context or wiz.Context()
     from .user import getEmailFromConfig, Step_Email
     email = getEmailFromConfig()
     if not email:
         class Step00(Step_Email):
             def nextStep(self):
-                nextstep = Step1_Workdir(self.context)
+                nextstep = Step1_Chemical_formula(self.context)
                 nextstep.show()
                 return
         start = Step00(context)
     else:
         context.email = email
-        start = Step1_Workdir(context)
+        start = Step1_Chemical_formula(context)
     start.show()
     return context
 
 
-class Step1_Workdir(wiz.Step_SelectDir):
-
-    header_text = "Please select the directory where the sample assembly will be saved"
-    instruction = 'Select workding directory'
-    context_attr_name = 'work_dir'
-    target_name = 'work'
-    newdir = True
-    def createNextStep(self):
-        return Step2_Name(self.context)
-
-    pass
-
-class Step2_Name(wiz.Step):
-
-    header_text = "Name of the sample assembly"
+class Step1_Chemical_formula(wiz.Step):
+    
+    header_text = "Material: chemical formula"
     
     def createHeader(self):
         return ipyw.HTML("<h3>%s</h3>" % self.header_text)
 
     def createBody(self):
-        self.text = ipyw.Text(value='mysample', description='Name')
-        self.body = ipyw.VBox(children=[self.select])
+        self.text = ipyw.Text(value="e.g. V2O3", description='Chemical formula')
+        self.body = ipyw.VBox(children=[self.text])
         return self.body
 
     def validate(self):
+        from mcvine.workflow.sampleassembly.scaffolding.utils import decode_chemicalformula
+        try: formula = decode_chemicalformula(self.text.value)
+        except Exception as ex:
+            self.updateStatusBar("Failed to decode chemical formula.\n" + str(ex))
+            return False
+        if not len(formula):
+            self.updateStatusBar("Empty formula\n")
+            return False
+        return True
+    
+    def createNextStep(self):
+        return Step2_Lattice_abcabg(self.context)
+
+
+class Lattice_abcabg(FormFactory):
+
+    P = FormFactory.P
+    parameters = [
+        P(name='a', label="a", widget=ipyw.FloatText("1.0")),
+        P(name='b', label="b", widget=ipyw.FloatText("1.0")),
+        P(name='c', label="c", widget=ipyw.FloatText("1.0")),
+        P(name='alpha', label="alpha", widget=ipyw.FloatText("90.0")),
+        P(name='beta', label="beta", widget=ipyw.FloatText("90.0")),
+        P(name='gamma', label="gamma", widget=ipyw.FloatText("90.0")),
+    ]
+
+    
+class Step2_Lattice_abcabg(wiz.Step):
+    
+    def createBody(self):
+        self.form_factory = Lattice_abcabg()
+        form = self.form_factory.createForm()
+        widgets= [form]
+        return ipyw.VBox(children=widgets)
+
+    def validate(self):
+        params = self.form_factory.inputs
+        # check user input
+        if not params:
+            self.updateStatusBar("Please check your inputs")
+            return False
+        # save user input
+        for k, v in params.items():
+            setattr(self.context, k, v)
+        return True
+    
+    def createNextStep(self):
+        return Step3_ShapeType(self.context)
+
+    pass
+
+
+class Step3_ShapeType(wiz.Step_SingleChoice):
+
+    def choices(self):
+        return ['sphere', 'block', 'cylinder']
+    def default_choice(self): return 'sphere'
+
+    def validate(self):
+        self.context.shape_type = self.select.value
         return True
     
     def nextStep(self):
@@ -77,12 +122,128 @@ class Step2_Name(wiz.Step):
         next_step.previous_step = self
         next_step.show()
         return
-
+                                                                            
     def createNextStep(self):
-        raise NotImplementedError
+        return Step3a_ShapeConfig(self.context)
+
+
+class Step3a_ShapeConfig(wiz.Step):
+    
+    def createHeader(self):
+        return ipyw.HTML("<h4>%s configuration</h4>" % self.context.shape_type)
+    
+    def createBody(self):
+        self.form_factory = eval(self.context.shape_type.capitalize())()
+        form = self.form_factory.createForm()
+        widgets= [form]
+        return ipyw.VBox(children=widgets)
+
+    def validate(self):
+        params = self.form_factory.inputs
+        # check user input
+        if not params:
+            self.updateStatusBar("Please check your inputs")
+            return False
+        self.context.params = params # save user input
+        return True
+    
+    def createNextStep(self):
+        return Step4_Workdir(self.context)
 
     pass
 
+
+class Shape(FormFactory):
+
+    P = FormFactory.P
+    parameters = []
+
+class Sphere(Shape):
+
+    P = FormFactory.P
+    parameters = Shape.parameters + [
+        P(name='radius', label="radius", widget=ipyw.Text("1.*cm")),
+    ]
     
+class Cylinder(Shape):
+
+    P = FormFactory.P
+    parameters = Shape.parameters + [
+        P(name='radius', label="radius", widget=ipyw.Text("1.*cm")),
+        P(name='height', label="height", widget=ipyw.Text("5.*cm")),
+    ]
+    
+class Block(Shape):
+
+    P = FormFactory.P
+    parameters = Shape.parameters + [
+        P(name='width', label="width", widget=ipyw.Text("1.*cm")),
+        P(name='height', label="height", widget=ipyw.Text("1.*cm")),
+        P(name='thickness', label="thickness", widget=ipyw.Text("1.*cm")),
+    ]
+    
+
+class Step4_Workdir(wiz.Step_SelectDir):
+
+    header_text = "Please select the directory where the sample will be saved"
+    instruction = 'Select workding directory'
+    context_attr_name = 'work_dir'
+    target_name = 'work'
+    newdir = True
+    def createNextStep(self):
+        return Step5_Name(self.context)
+    pass
+
+
+class Step5_Name(wiz.Step):
+
+    header_text = "Name of the sample"
+    
+    def createHeader(self):
+        return ipyw.HTML("<h3>%s</h3>" % self.header_text)
+
+    def createBody(self):
+        self.text = ipyw.Text(value='mysample', description='Name')
+        self.body = ipyw.VBox(children=[self.text])
+        return self.body
+
+    def validate(self):
+        self.context.name = self.text.value
+        return True
+    
+    def createNextStep(self):
+        return Step6_Confirmation(self.context)
+
+    pass
+
+
+class Step6_Confirmation(wiz.Step):
+
+    def createHeader(self):
+        return ipyw.HTML("<h4>Confirmation</h4>")
+    
+    def createBody(self):
+        labels = ['name']
+        values = [self.context.name]
+        labels_html = ipyw.HTML("\n".join("<p>%s</p>" % l for l in labels))
+        values_html = ipyw.HTML("\n".join("<p>%s</p>" % l for l in values))
+        info = ipyw.HBox(
+            children=[labels_html, values_html],
+            layout=ipyw.Layout(padding="5px", border="1px inset #eee"))
+        info.add_class("info")
+        panel = ipyw.VBox(children=[info])
+        return panel
+
+    def validate(self):
+        return True
+
+    def nextStep(self):
+        return self.generate()
+    
+    def generate(self):
+        return
+
+    pass
+
 
 # End of file 
