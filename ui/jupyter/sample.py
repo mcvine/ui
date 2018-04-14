065f335ef70c0b98d6975c903b65e1f574584d98
Jiao Lin <linjiao@ornl.gov>
2018-04-13 22:24:13 -0400

    Refs #13. supported shapes "hollowCylinder" and "sphereShell". refactored sample.py so that shapes are handled in shape.py

--- ui/jupyter/sample.py
+++ ui/jupyter/sample.py
@@ -116,93 +116,10 @@ class Step2_Lattice_abcabg(wiz.Step):
         return True
     
     def createNextStep(self):
+        from .shape import Step3_ShapeType
         return Step3_ShapeType(self.context)
 
     pass
-
-
-class Step3_ShapeType(wiz.Step_SingleChoice):
-
-    def choices(self):
-        return ['sphere', 'block', 'cylinder']
-    def default_choice(self): return 'sphere'
-
-    header_text = 'Shape'
-    def createHeader(self):
-        return ipyw.HTML("<h3>%s</h3>" % self.header_text)
-    
-    def validate(self):
-        self.context.shape_type = self.select.value
-        return True
-    
-    def nextStep(self):
-        # need to overload this function, since the next step depends on
-        # the choice in this step
-        self.next_step = next_step = self.createNextStep()
-        next_step.previous_step = self
-        next_step.show()
-        return
-                                                                            
-    def createNextStep(self):
-        return Step3a_ShapeConfig(self.context)
-
-
-class Step3a_ShapeConfig(wiz.Step):
-    
-    def createHeader(self):
-        return ipyw.HTML("<h4>%s configuration</h4>" % self.context.shape_type)
-    
-    def createBody(self):
-        self.form_factory = eval(self.context.shape_type.capitalize())()
-        form = self.form_factory.createForm()
-        widgets= [form]
-        return ipyw.VBox(children=widgets)
-
-    def validate(self):
-        params = self.form_factory.inputs
-        # check user input
-        if not params:
-            self.updateStatusBar("Please check your inputs")
-            return False
-        self.context.shape_params = params # save user input
-        return True
-    
-    def createNextStep(self):
-        from .excitation import Step4_ExcitationType
-        return Step4_ExcitationType(self.context)
-
-    pass
-
-
-class Shape(FormFactory):
-
-    P = FormFactory.P
-    parameters = []
-
-class Sphere(Shape):
-
-    P = FormFactory.P
-    parameters = Shape.parameters + [
-        P(name='radius', label="radius", widget=ipyw.Text("1.*cm"), converter=str),
-    ]
-    
-class Cylinder(Shape):
-
-    P = FormFactory.P
-    parameters = Shape.parameters + [
-        P(name='radius', label="radius", widget=ipyw.Text("1.*cm"), converter=str),
-        P(name='height', label="height", widget=ipyw.Text("5.*cm"), converter=str),
-    ]
-    
-class Block(Shape):
-
-    P = FormFactory.P
-    parameters = Shape.parameters + [
-        P(name='width', label="width", widget=ipyw.Text("1.*cm"), converter=str),
-        P(name='height', label="height", widget=ipyw.Text("1.*cm"), converter=str),
-        P(name='thickness', label="thickness", widget=ipyw.Text("1.*cm"), converter=str),
-    ]
-    
 
 
 class Step5_Workdir(wiz.Step_SelectDir):
